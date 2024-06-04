7b8ba5b256dbbcf6dad4e4ff280e566bc6c02a00
Fabricio Veronez <fabricioveronez@gmail.com>
2024-06-04 09:40:02 -0300

    Vamos

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.route('/').get((req, res) => {
-    res.send('<h1>Aplicacao exemplo</h1>');
+    res.send('<h1>Aplicacao exemplo - versão 2.0</h1>');
 });
 
 app.listen(8080, () => {
